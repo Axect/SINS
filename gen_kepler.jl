9b1f9e44f4359b5cf29f039e05306093d3131d46
axect <axect@outlook.kr>
2021-09-10 06:32:35 +0900

    Data Generation complete

--- gen_kepler.jl
+++ gen_kepler.jl
@@ -1,39 +1,65 @@
 using DifferentialEquations
 using DataFrames, NCDataFrame
-using YAActL
 using Distributions
 
 size = parse(Int64, ARGS[1])
 
-function kepler2d!(du, u, _p, _t)
-    du[1] = u[3]
-    du[2] = u[4]
-    r = sqrt(u[1]^2 + u[2]^2)
-    du[3] = -u[1]/r^3
-    du[4] = -u[2]/r^3
+function H_velo!(du, v, u, p, t)
+    dx, dy = v
+    du[1] = dx
+    du[2] = dy
+end
+
+function H_accel!(dv, v, u, p, t)
+    x, y = u
+    r = (x^2 + y^2)^(3/2)
+    dv[1] = -x/r
+    dv[2] = -y/r
 end
 
 tspan = (0.0, 10.0)
 
 r_dist = Uniform(1.0, 2.0)
 th_dist = Uniform(0.0, 2.0*pi)
+#e_dist = Normal(0.0, pi/12)
 vx_dist = Uniform(-0.2, -0.1)
 vy_dist = Uniform(0.1, 0.2)
 
 r = rand(r_dist, size)
 th = rand(th_dist, size)
+#e = rand(e_dist, size)
 x = r .* cos.(th)
 y = r .* sin.(th)
 vx = rand(vx_dist, size)
 vy = rand(vy_dist, size)
+#vx = -0.1 * r .* cos.(th .+ e)
+#vy = 0.1 * r .* sin.(th .+ e)
 
 z = hcat(x, y, vx, vy)
 
 for i = 1:size
-    u_init = z[i,:]
-    prob = ODEProblem(kepler2d!, u_init, tspan)
-    sol = solve(prob, saveat=0.1)
-    sol_u = vcat(sol.u'...)
+    a, b, va, vb = z[i,:]
+    if abs(a/b - va/vb) < 0.1
+        va = -va
+    end
+    prob = DynamicalODEProblem(
+        H_accel!,
+        H_velo!,
+        [va, vb],
+        [a, b],
+        (0.0, 10.0)
+    )
+    sol = solve(prob, DPRKN6(), saveat=0.1) # Sympletic integrator
+
+    sol_u = zeros(length(sol), 4)
+    for i in 1:length(sol)
+        va, vb, a, b = sol.u[i]
+        sol_u[i,1] = a
+        sol_u[i,2] = b
+        sol_u[i,3] = va
+        sol_u[i,4] = vb
+    end
+
     df = DataFrame(
         "t" => sol.t,
         "x" => sol_u[:,1],
